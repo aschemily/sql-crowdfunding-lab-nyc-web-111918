5090673095d0fb1416fe956179e400c544a66ba8
Emily Asch <e.asch@me.com>
2018-11-29 21:38:37 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO projects (title, funding_goal, start_date, end_date) VALUES ("Go Green", 2000, "December 2018", "February 2019");
+INSERT INTO projects (title, funding_goal, start_date, end_date) VALUES ("Recycle", 100, "March 2018", "June 2018");
+INSERT INTO projects (title, funding_goal, start_date, end_date) VALUES ("Ocean", 300, "June 2015", "July 2015");
+INSERT INTO projects (title, funding_goal, start_date, end_date) VALUES ("Planet Earth", 450, "April 2002", "May 2002");
+INSERT INTO projects (title, funding_goal, start_date, end_date) VALUES ("Public Schools", 40, "January 2009", "February 2009");
+INSERT INTO projects (title, funding_goal, start_date, end_date) VALUES ("Girls Who Code", 3000, "June 2018", "August 2018")
+INSERT INTO projects (title, funding_goal, start_date, end_date) VALUES ("Happy Farm", 5000, "August 2018", "September 2019");
+INSERT INTO projects (title, funding_goal, start_date, end_date) VALUES ("Ocean Animals", 700, "September 2018", "December 2018");
+INSERT INTO projects (title, funding_goal, start_date, end_date) VALUES ("Animals", 400, "November 2018", "December 2018")
+INSERT INTO projects (title, funding_goal, start_date, end_date) VALUES ("Eco", 100, "December 2018", "February 2018")
+
